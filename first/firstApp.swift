f1913d91c21cb1a47ff6e5715595564ed2a6ef5f
vijayganesh-sde <vijayganeshsreeram2003@gmail.com>
2022-03-31 10:25:52 +0530

    Initial Commit

---
+++ first/firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstApp.swift
+//  first
+//
+//  Created by SR Vijay Ganesh on 31/03/22.
+//
+
+import SwiftUI
+
+@main
+struct firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
